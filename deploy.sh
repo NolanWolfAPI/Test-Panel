b87e28786af7784b38093835d13ccd4b8b045dcd
Jordan Wood <jordan@elitehosting.io>
2019-05-15 22:30:48 +0100

    Change how deploy works

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
+docker build -t mrblnetwork/panel .
+docker tag mrblnetwork/panel mrblnetwork/panel:$TRAVIS_TAG
+docker push mrblnetwork/panel:$TRAVIS_TAG
