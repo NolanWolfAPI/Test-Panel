1b96c8573961e6ce34209805fb08fdeff60fed65
Jordan Wood <jordan@elitehosting.io>
2019-05-15 22:51:22 +0100

    Create latest

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USER" --password-stdin
 docker build -t mrblnetwork/panel .
+docker push mrblnetwork/panel
 docker tag mrblnetwork/panel mrblnetwork/panel:$TRAVIS_TAG
 docker push mrblnetwork/panel:$TRAVIS_TAG
