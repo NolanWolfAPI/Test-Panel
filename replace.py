4cde8e7fb4b841bc7773da09658468ec78979fb9
Jordan Wood <jordan@elitehosting.io>
2019-05-17 14:32:23 +0100

    Add missing comma

--- replace.py
+++ replace.py
@@ -4,7 +4,7 @@ import os
 # Setup environment
 old = 'var environment = {\n    name: \'temp\',\n    production: true,\n    title: \'temp\',\n    baseAuthURL: ' \
       '\'temp\',\n    baseAPIURL: \'temp\',\n};'
-new = 'var environment = {name: \'Marble\', production: true, title: \'Marble Panel\' baseAuthURL: \'' \
+new = 'var environment = {name: \'Marble\', production: true, title: \'Marble Panel\', baseAuthURL: \'' \
       + os.environ['BASE_AUTH_URL'] + '\', baseAPIURL: \'' + os.environ['BASE_API_URL'] + '\',};'
 
 s = open(os.environ['FILE_PATH']).read()
